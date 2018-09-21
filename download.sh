628b420671f4799b3660830110ae799d16e204d4
Tianyi Luo <pkuluotianyi@gmail.com>
2018-09-20 18:03:18 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Download SQuAD
-SQUAD_DIR=~/data/squad
-mkdir -p $SQUAD_DIR
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
+#SQUAD_DIR=~/data/squad
+#mkdir -p $SQUAD_DIR
+#wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
+#wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
 
 # Download GloVe
 GLOVE_DIR=~/data/glove
